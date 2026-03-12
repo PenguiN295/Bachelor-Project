005ac6cd8d6bbec3e0305401d6a19dcf2623575e
Adrian <adrianghiorma08@gmail.com>
2026-03-12 16:25:32 +0200

    added an open in google maps button for events

--- Frontend/react-frontend/src/components/MapComponent.tsx
+++ Frontend/react-frontend/src/components/MapComponent.tsx
@@ -4,14 +4,15 @@ import { useMapData } from "../hooks/useMapData";
 import LoadingState from "./LoadingState";
 import { MapPicker } from "./MapPicker";
 import { useEffect } from "react";
+import { href } from "react-router";
 
 interface MapProps {
     position: { lat: number, lng: number };
     readOnly?: boolean;
-    onPositionChange?: (pos: { lat: number; lng: number, city: string, county: string }, ) => void;
+    onPositionChange?: (pos: { lat: number; lng: number, city: string, county: string },) => void;
 }
 
-const MapComponent: React.FC<MapProps> = ({ position,readOnly,onPositionChange }) => {
+const MapComponent: React.FC<MapProps> = ({ position, readOnly, onPositionChange }) => {
     const { getAdress: addressData, isLoading } = useMapData(position);
     useEffect(() => {
         if (addressData && onPositionChange) {
@@ -32,12 +33,20 @@ const MapComponent: React.FC<MapProps> = ({ position,readOnly,onPositionChange }
                         Location: {addressData ? `${addressData.city}, ${addressData.principalSubdivision}` : 'Loading...'}
                     </div>
                     <MapContainer center={position} zoom={13} style={{ height: '300px' }}>
+                        <a className="btn btn-secondary"  
+                        href = {"https://www.google.com/maps/search/?api=1&query=" + position.lat + "," + position.lng} rel="noopener noreferrer"
+                        target="_blank"
+                        style={{ position: 'absolute', top: 10, right: 10, zIndex: 1000 }}>
+                            View on Google Maps
+                            
+                        </a>
                         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
                         <LocationMarker position={position} />
                         {!readOnly && onPositionChange && (
                             <MapPicker onLocationSelect={(lat, lng) => onPositionChange({ lat, lng, city: addressData?.city || '', county: addressData?.county || addressData?.principalSubdivision || '' })} />
                         )}
                     </MapContainer>
+
                 </div>
             )}
         </div>
